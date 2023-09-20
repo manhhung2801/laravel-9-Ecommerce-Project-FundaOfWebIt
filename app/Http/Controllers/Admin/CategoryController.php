3bc5ed5cdf65ece8620c1136f5c981e30dcdcd0f
manhhung2801 <hung28012002zz@gmail.com>
2023-09-20 14:05:37 +0700

    Category View, edit update, confirm delete useing livewire

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Support\Str;
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\File;
 use App\Http\Requests\CategoryFormRequest;
+
 
 class CategoryController extends Controller
 {
@@ -47,5 +49,43 @@ class CategoryController extends Controller
         $category->save();
 
         return redirect('admin/category')->with('message', 'Category Added Successfully');
+    }
+
+    public function edit(Category $category){
+        return view('admin.category.edit', compact('category'));
+    }
+    public function update(CategoryFormRequest $request, $category){
+
+        $validateData = $request->validate($request->rules());
+
+        $category = Category::findOrFail($category);
+
+        $category->name = $validateData['name'];
+        $category->slug = Str::slug($validateData['slug']);
+        $category->description = $validateData['description'];
+
+        if($request->hasFile('image')){
+            $path = 'uploads/category/'.$category->image;
+            if(File::exists($path)){
+                File::delete($path);
+            }
+            $file = $request->file('image');
+
+            $ext = $file->getClientOriginalExtension();
+            $filename = time().'.'.$ext;
+
+            $file->move('uploads/category/', $filename);
+
+            $category->image = $filename;
+        }
+
+        $category->meta_title = $validateData['meta_title'];
+        $category->meta_keyword = $validateData['meta_keyword'];
+        $category->meta_description = $validateData['meta_description'];
+
+        $category->status = $request->status == true ? '1':'0';
+        $category->update();
+
+        return redirect('admin/category')->with('message', 'Category Updated Successfully');
     }
 }
